652c92002907f54ce951bb2cc75cd7c70caac83b
Mahbub <mahbub.095@gmail.com>
2020-04-04 11:55:05 +0600

    String

---
+++ chapter_five/string.js
@@ -0,0 +1,21 @@
+//var a= 'abc'
+//var b= 'def'
+
+//console.log(a<b)
+//console.log('a'>'Z')
+ // var length =0
+// var n=n.length
+
+  // while(true){
+  //     if(a.charAt(length) =''){
+  //       break
+  //     }else{
+  //       length++
+  //     }
+  // } 
+   
+  var str = "Hello World!"
+  var n = str.length;
+  
+ console.log(str);
+   
