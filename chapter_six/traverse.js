e9db40b4e308e3703def6797ef2905232ead113c
Mahbub <mahbub.095@gmail.com>
2020-04-04 13:03:42 +0600

    Traverse Array

---
+++ chapter_six/traverse.js
@@ -0,0 +1,12 @@
+var arr = [1,2,3,4,5,6,7,8]
+/*
+for(var i=0;i<arr.length;i++){
+     arr[i]=arr[i]+2
+}
+console.log(arr)
+*/
+var sum = 0
+for (var i=0;i<arr.length;i++){
+    sum =sum+ arr[i]
+}
+console.log(sum)
