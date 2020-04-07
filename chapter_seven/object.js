b361c7131a698b7f81484b07cda88d6eb4be92d9
Mahbub <mahbub.095@gmail.com>
2020-04-08 00:54:41 +0600

    Object

---
+++ chapter_seven/object.js
@@ -0,0 +1,18 @@
+var point ={
+	x: 10,
+	y: 20,
+	z: 35
+}
+
+ 
+console.log(point.x)
+console.log(point.y)
+console.log(point.x + point.z)
+
+
+console.log(point['x'])
+console.log(point['z'])
+
+var  show = 'z'
+console.log(point[show])
+ 
