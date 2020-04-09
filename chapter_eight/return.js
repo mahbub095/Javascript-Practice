033ad7f4bc25431c5ffd3c9b84cb3f438bbfba4f
Mahbub <mahbub.095@gmail.com>
2020-04-09 13:29:57 +0600

    Pure js

--- chapter_eight/return.js
+++ chapter_eight/return.js
@@ -1,11 +1,18 @@
-function add(){
-    var sum = 0
-    for(var i =0;i<arguments.length;i++){
-        sum +=arguments[i]
-    }
-    return sum
+// function add(){
+//     var sum = 0
+//     for(var i =0;i<arguments.length;i++){
+//         sum +=arguments[i]
+//     }
+//     return sum
 
+// }
+
+// var result = add(1,2,3)
+// console.log(result)
+
+
+function add(a,b){
+    c=a+b
 }
-
-var result = add(1,2,3)
-console.log(result)
+add(50,50)
+console.log(c)
---
+++ chapter_nine/pure.js
@@ -0,0 +1,22 @@
+function sqr(n)
+{
+    return n*n
+}
+
+ console.log(sqr(3))
+ console.log(sqr(3))
+ console.log(sqr(3))
+
+
+ var point = {
+     x:0,
+     y:0
+ }
+
+ function printPoint(point){
+     point.x =100
+     point.y =200
+     console.log(point)
+ }
+ printPoint(point)
+ console.log(point)
